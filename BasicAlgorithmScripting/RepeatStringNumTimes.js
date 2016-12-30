193b4633a5cbd0bf1e7f498ae1315f325feec6e8
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-29 22:27:08 -0500

    Update RepeatStringNumTimes.js

--- BasicAlgorithmScripting/RepeatStringNumTimes.js
+++ BasicAlgorithmScripting/RepeatStringNumTimes.js
@@ -1,7 +1,8 @@
 
 /*
 
-Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive
+number.
 
 Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
