847ec8512f0ab393e1675c5b95510242c21dd02c
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-01-12 14:46:03 -0500

    Create BasicServerWithLogging.js

---
+++ Expressjs/UnderstandingExpressjs/BasicServerWithLogging.js
@@ -0,0 +1,38 @@
+var http = require("http");
+var express = require("express");
+var logger = require("morgan");
+
+// Build the app
+var app = express();
+
+//Logging middleware
+app.use(logger("combined"));
+  //Fun Fact: logger() returns a function.
+
+// Homepage
+app.use(function(request, response, next){
+  if(request.url == "/") {
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.end("Welcome to the homepage!\n");
+  } else {
+    next();
+  }
+});
+
+// About page
+app.use(function(request, response, next){
+  if(request.url == "/about") {
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.end("Welcome to the about page!\n");
+  } else {
+    next();
+  }
+});
+
+// 404'd
+app.use(function(request, response){
+  response.writeHead(404, {"Content-Type" : "text/plain"});
+  response.end("404 error!\n");
+});
+
+http.createServer(app).listen(1337);
